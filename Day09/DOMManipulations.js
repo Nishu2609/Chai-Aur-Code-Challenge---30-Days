a072a237424629533e404f8948cdc85d17c0a872
soniinfo <soniinfo2008@gmail.com>
2024-07-21 12:17:23 +0530

    Day 09 Completed

--- Day09/DOMManipulations.js
+++ Day09/DOMManipulations.js
@@ -40,11 +40,62 @@ ul.appendChild(li);
 // }
  
 // Task 02
-let output2 = document.getElementById("output");
+// let output2 = document.getElementById("output");
+// if (output2) {
+//   output2.remove();
+// } else {
+//   console.error("Element with id 'output2' not found.");
+// }
+
+// Task 03 Modifying attributes and classes
+if(document.getElementById("output2")){
+let num1 = document.getElementById("num1");
+if (num1) {
+  num1.setAttribute("placeholder", "Enter a number1 e.g 5");
+} else {
+  console.error("Element with id 'num1' not found.");
+}
+let num2 = document.getElementById("num2");
+if (num2) {
+  num2.setAttribute("placeholder", "Enter a number2 e.g 10");
+} else {
+  console.error("Element with id 'num2' not found.");
+}   
+}
+else {
+    console.error("Element with id 'output2' not found.");
+}
+// Task 04 Modifying styles
+let output2 = document.getElementById("output2");
 if (output2) {
-  output2.remove();
+  output2.style.backgroundColor = "yellow";
 } else {
   console.error("Element with id 'output2' not found.");
 }
+
+// Task 05 Event handling click on btn then chage text of paragraph.
+
+let button = document.getElementById("btn");
+if (button) {
+  button.addEventListener("click", function() {
+    document.getElementById("para").innerHTML = "Day-09 of Chai Daur Code 30 Days Nishant's Challenge.";
+  });
+}
+else {
+    console.error("Element with id 'btn' not found.");
+}
+
+// on mouseover change the border color of para
+let para = document.getElementById("para");
+if (para) {
+  para.addEventListener("mouseover", function() {
+    para.style.borderColor = "blue";
+  });
+}
+else {
+    console.error("Element with id 'para' not found.");
+}
+
+
 }); 
  
